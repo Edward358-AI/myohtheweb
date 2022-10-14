ca38b3edb565d2fa10b944c6830cdba1edef7e8e
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-10-14 00:56:12 +0000

    ddydjrhf6uyfky

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import process from 'node:process';
 import('./index.mjs');
+const unhandledRejections = new Map();
+process.on('unhandledRejection', (reason, promise) => {
+  unhandledRejections.set(promise, reason);
+});
+process.on('rejectionHandled', (promise) => {
+  unhandledRejections.delete(promise);
+});
