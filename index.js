6a3a30211294e5d6e173d276288195259b7fbfa2
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-11-07 05:04:18 +0000

    d

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-import process from 'node:process';
-import('./index.mjs');
-const unhandledRejections = new Map();
-process.on('unhandledRejection', (reason, promise) => {
-  unhandledRejections.set(promise, reason);
+import createServer from '@tomphttp/bare-server-node';
+import http from 'http';
+import nodeStatic from 'node-static';
+
+
+const bare =  createServer('/bare/');
+const serve = new nodeStatic.Server('static/');
+const PORT = process.env.PORT || 3030;
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
+	} else {
+		serve.serve(req, res);
+	}
 });
-process.on('rejectionHandled', (promise) => {
-  unhandledRejections.delete(promise);
+
+server.on('upgrade', (req, socket, head) => {
+	if (bare.shouldRoute(req, socket, head)) {
+		bare.routeUpgrade(req, socket, head);
+	}else{
+		socket.end();
+	}
+});
+
+server.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
